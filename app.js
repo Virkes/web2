a4a6252eb640ea944863832b35c773c934f0c616
Virkes <kv006504150@fer.hr>
2022-10-30 19:02:03 +0100

    Changed app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 const externalUrl = process.env.RENDER_EXTERNAL_URL;
-const port = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 4080;
+const port = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 const config = {
   authRequired: false,
